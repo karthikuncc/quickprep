cbd7524cb458849e6496083868b80e580d37c811
Karthik Gorijavolu <scoda@Karthiks-MBP.fios-router.home>
2019-11-23 22:52:29 -0500

    second

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import Todo from './Components/Todo/Todo'
 import './App.css';
+import TodoForm from './Components/Todo/TodoForm';
 
 function App() {
+  const [todos,setTodos] = useState([
+    { 
+      text:"Learn about React",
+      isCompleted:false
+    },
+    {
+      text:'Meet friend for lunch',
+      isCompleted:false
+    },
+    {
+      text:'Build really cool todo',
+      isCompleted:false
+    }
+  ]);
+
+  const addTodo = text =>{
+    const NewTodos =[...todos,{text}];
+    setTodos(NewTodos);
+  }
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted =true;
+    setTodos(newTodos);
+  }
+
+   const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index,1);
+    setTodos(newTodos);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {todos.map((todo,index) =>(
+          <Todo 
+              key={index} 
+              index={index} 
+              todo={todo} 
+              completeTodo={completeTodo}
+              removeTodo ={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+
+      </div>
     </div>
   );
 }
